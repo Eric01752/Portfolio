0603a0553c011b8e76e7dd56915d6a4c799837b8
Eric Schmidt <eric.schmidt1350@gmail.com>
2022-03-01 11:28:29 -0500

    Fix

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'MERN Social Media',
     description:
       'Social Media application using React, Node.js, Express, MongoDB, and Socket.io.',
-    image: '/images/MERN_SocialMedia.jpg',
+    image: '/images/MERN_SocialMedia.JPG',
     tags: ['Mongo', 'Express', 'React', 'Node', 'Socket.io'],
     source: 'https://github.com/Eric01752/MERN_SocialMedia',
     visit: 'https://eric-social-media.herokuapp.com',
@@ -13,7 +13,7 @@ export const projects = [
     title: 'MERN SocialDev',
     description:
       'Small social media app for developers. Uses React, Redux, Express, Node, MongoDB.',
-    image: '/images/MERN_SocialDev.jpg',
+    image: '/images/MERN_SocialDev.JPG',
     tags: ['React', 'Redux', 'Express', 'Node', 'Mongo'],
     source: 'https://github.com/Eric01752/MERN-SocialDev',
     visit: 'https://eric-social-dev.herokuapp.com',
@@ -23,7 +23,7 @@ export const projects = [
     title: 'MERN Shopping List',
     description:
       'Small shopping list app that uses React, Redux, Express, Node, and MongoDB.',
-    image: '/images/MERN_ShoppingList.jpg',
+    image: '/images/MERN_ShoppingList.JPG',
     tags: ['React', 'Redux', 'Express', 'Node', 'Mongo'],
     source: 'https://github.com/Eric01752/MERN-Shopping-List-App',
     visit: 'https://eric-shopping-list.herokuapp.com',
